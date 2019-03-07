d5e2d2db5f07cb9fbb6395a47617edb63462d5ef
DonTheChimera <donaldtbradfield@gmail.com>
2019-03-07 14:56:55 -0600

    it is coming along nicely now

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,6 @@ import { TopmostComponent } from './nav/topmost/topmost.component';
 import { MiddleComponent } from './nav/middle/middle.component';
 import { AboutComponent } from './nav/about/about.component';
 import { LandingComponent } from './landing/landing.component';
-import { IndustriesComponent } from './nav/about/industries/industries.component';
-import { ServicesComponent } from './nav/about/services/services.component';
-import { LatestComponent } from './nav/about/latest/latest.component';
 import { AboutPageComponent } from './about-page/about-page.component';
 import { FootComponent } from './footer/foot/foot.component';
 
@@ -20,9 +17,6 @@ import { FootComponent } from './footer/foot/foot.component';
     MiddleComponent,
     LandingComponent,
     AboutComponent,
-    IndustriesComponent,
-    ServicesComponent,
-    LatestComponent,
     AboutPageComponent,
     FootComponent
   ],
